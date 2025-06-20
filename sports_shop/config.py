709ac21b3525d07ea64bcffb86780792e9390d80
Samangelof <samangelofw@gmail.com>
2025-06-20 14:40:10 +0300

    fix.

--- sports_shop/config.py
+++ sports_shop/config.py
@@ -1,5 +1,4 @@
 import os
 
-
 APP_NAME = os.getenv("APP_NAME", "Sports Shop")
 GITHUB_TOKEN = os.getenv("APP_NAME", "GITHUB_TOKEN_NOT_FOUND")
--- sports_shop/start.py
+++ sports_shop/start.py
@@ -1,5 +1,4 @@
 from sports_shop.main import main
 
-
 if __name__ == "__main__":
     main()
